42170f41d91870811ff2cf8b71eb36021fb59c25
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-02-26 18:59:22 +0900

    WIP

--- propertykey.go
+++ propertykey.go
@@ -5,6 +5,14 @@ type PropertyKey struct {
 	PID uint32
 }
 
-func DefinePropertyKey() (p *PropertyKey) {
-	return
+func DefinePropertyKey(l uint32, w1, w2 uint16, b1, b2, b3, b4, b6, b7, b8 byte, pid uint32) *PropertyKey {
+	g := ole.GUID{
+		Data1: l,
+		Data2: w1,
+		Data3: w2,
+		Data4: [8]byte{b1, b2, b3, b4, b5, b6, b7, b8},
+	}
+	p := PropertyKey{g, pid}
+
+	return &p
 }
