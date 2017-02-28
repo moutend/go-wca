d503cd5292d1a3ebd68b3a9f265e7bace67aa936
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-02-28 09:53:28 +0900

    Add

---
+++ immdevice_func.go
@@ -0,0 +1,27 @@
+// +build !windows
+
+package main
+
+import (
+	"reflect"
+	"syscall"
+	"unsafe"
+
+	"github.com/go-ole/go-ole"
+)
+
+func mmdActivate(mmd *IMMDevice, refIID *ole.GUID, ctx uint32, prop, obj interface{}) (err error) {
+	return ole.NewError(E_NOTIMPL)
+}
+
+func mmdOpenPropertyStore(mmd *IMMDevice, storageMode uint32, ps **IPropertyStore) (err error) {
+	return ole.NewError(E_NOTIMPL)
+}
+
+func mmdGetId(mmd *IMMDevice, strId *uint32) (err error) {
+	return ole.NewError(E_NOTIMPL)
+}
+
+func mmdGetState(mmd *IMMDevice, state *uint32) (err error) {
+	return ole.NewError(E_NOTIMPL)
+}
