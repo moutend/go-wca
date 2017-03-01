1149bcf7725c4b86e81a5bb17ba9ef5f40d6f7bd
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-03-01 17:21:28 +0900

    Add property key

--- propertykey.go
+++ propertykey.go
@@ -9,6 +9,10 @@ type PropertyKey struct {
 	PID uint32
 }
 
+var (
+	PKEY_Device_FriendlyName = DefinePropertyKey(0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0, 14) // DEVPROP_TYPE_STRING
+)
+
 func DefinePropertyKey(l uint32, w1, w2 uint16, b1, b2, b3, b4, b5, b6, b7, b8 byte, pid uint32) PropertyKey {
 	g := ole.GUID{
 		Data1: l,
