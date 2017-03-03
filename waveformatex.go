89e24f30524eb39aeb982ec448962b05385a1899
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-03-03 17:27:06 +0900

    Add

---
+++ waveformatex.go
@@ -0,0 +1,11 @@
+package wca
+
+type WAVEFORMATEX struct {
+	WFormatTag      uint16
+	NChannels       uint16
+	NSamplesPerSec  uint32
+	NAvgBytesPerSec uint32
+	NBlockAlign     uint16
+	WBitsPerSample  uint16
+	CbSize          uint16
+}
