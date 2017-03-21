2091d9fb820bc1ae3b293e0d6d56418016cbfa11
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-03-21 11:37:25 +0900

    Add functions

--- com_func.go
+++ com_func.go
@@ -6,6 +6,14 @@ import (
 	"github.com/go-ole/go-ole"
 )
 
-func CreateEventEx(securityAttributes, name, flag, desiredAccess uint32) (err error) {
+func CreateEventExA(securityAttributes, name, flag, desiredAccess uint32) (err error) {
+	return ole.NewError(ole.E_NOTIMPL)
+}
+
+func CloseHandle(hObject uintptr) (err error) {
+	return ole.NewError(ole.E_NOTIMPL)
+}
+
+func CoCreateInstance(clsid *ole.GUID, punk uintptr, clsctx uint32, iid *ole.GUID, obj interface{}) (err error) {
 	return ole.NewError(ole.E_NOTIMPL)
 }
--- com_windows.go
+++ com_windows.go
@@ -2,22 +2,49 @@
 package wca
 
 import (
-	"github.com/go-ole/go-ole"
+	"fmt"
+	"reflect"
 	"syscall"
+	"unsafe"
+
+	"github.com/go-ole/go-ole"
 )
 
 var (
-	modkernel32, _       = syscall.LoadDLL("kernel32.dll")
-	procCreateEventEx, _ = modkernel32.FindProc("CreateEventEx")
+	modkernel32, _ = syscall.LoadDLL("kernel32.dll")
+	modole32, _    = syscall.LoadDLL("ole32.dll")
+
+	procCreateEventExA, _   = modkernel32.FindProc("CreateEventExA")
+	procCloseHandle, _      = modkernel32.FindProc("CloseHandle")
+	procCoCreateInstance, _ = modole32.FindProc("CoCreateInstance")
 )
 
-func CreateEventEx(securityAttributes, name, flag, desiredAccess uint32) (err error) {
-	hr, _, _ := procCreateEventEx.Call(
-		uintptr(securityAttributes),
-		uintptr(name),
+func CreateEventExA(securityAttributes, name uintptr, flag, desiredAccess uint32) (hr uintptr) {
+	hr, _, _ = procCreateEventExA.Call(
+		securityAttributes,
+		name,
 		uintptr(flag),
 		uintptr(desiredAccess))
-	if hr != 0 {
+	return
+}
+
+func CloseHandle(hObject uintptr) (err error) {
+	hr, _, _ := procCloseHandle.Call(hObject)
+	if hr == 0 {
+		err = fmt.Errorf("unexpected error: call GetLastError to get details")
+	}
+	return
+}
+
+func CoCreateInstance(clsid *ole.GUID, punk uintptr, clsctx uint32, iid *ole.GUID, obj interface{}) (err error) {
+	objValue := reflect.ValueOf(obj).Elem()
+	hr, _, _ := procCoCreateInstance.Call(
+		uintptr(unsafe.Pointer(clsid)),
+		punk,
+		uintptr(clsctx),
+		uintptr(unsafe.Pointer(iid)),
+		objValue.Addr().Pointer())
+	if hr != ole.S_OK {
 		err = ole.NewError(hr)
 	}
 	return
