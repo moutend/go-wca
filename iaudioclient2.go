19f45ce6921f7891863c2910e05872208639d550
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-05-02 06:21:24 +0900

    Fix hierarchy of inheritance

--- iaudioclient2.go
+++ iaudioclient2.go
@@ -2,20 +2,16 @@ package wca
 
 import (
 	"unsafe"
-
-	"github.com/go-ole/go-ole"
 )
 
 // IAudioClient2 struct corresponds to IAudioClient2 interface.
 // See MSDN's documentation:
 // https://msdn.microsoft.com/en-us/library/windows/desktop/hh404179(v=vs.85).aspx
 type IAudioClient2 struct {
-	ole.IUnknown
 	IAudioClient
 }
 
 type IAudioClient2Vtbl struct {
-	ole.IUnknownVtbl
 	IAudioClientVtbl
 	IsOffloadCapable    uintptr
 	SetClientProperties uintptr
