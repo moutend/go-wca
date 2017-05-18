5e95a4e42f5659112d028749a38470fe99443259
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-05-19 08:24:59 +0900

    WIP

--- immnotificationclient.go
+++ immnotificationclient.go
@@ -1,44 +1,16 @@
 package wca
 
-import (
-	"unsafe"
-
-	"github.com/go-ole/go-ole"
-)
-
 type IMMNotificationClient struct {
-	ole.IUnknown
+	VTable *IMMNotificationClientVtbl
 }
 
 type IMMNotificationClientVtbl struct {
-	ole.IUnknownVtbl
+	QueryInterface         uintptr
+	AddRef                 uintptr
+	Release                uintptr
 	OnDeviceStateChanged   uintptr
 	OnDeviceAdded          uintptr
 	OnDeviceRemoved        uintptr
 	OnDefaultDeviceChanged uintptr
 	OnPropertyValueChanged uintptr
-}
-
-func (v *IMMNotificationClient) VTable() *IMMNotificationClientVtbl {
-	return (*IMMNotificationClientVtbl)(unsafe.Pointer(v.RawVTable))
-}
-
-func (v *IMMNotificationClient) OnDeviceStateChanged() (err error) {
-	return
-}
-
-func (v *IMMNotificationClient) OnDeviceAdded() (err error) {
-	return
-}
-
-func (v *IMMNotificationClient) OnDeviceRemoved() (err error) {
-	return
-}
-
-func (v *IMMNotificationClient) OnDefaultDeviceChanged() (err error) {
-	return
-}
-
-func (v *IMMNotificationClient) OnPropertyValueChanged() (err error) {
-	return
 }
