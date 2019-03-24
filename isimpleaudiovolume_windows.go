331bb83def75774b1206fc426c8454bf6039eb80
medusalix <ss7@live.de>
2019-03-24 15:42:44 +0100

    Fix import

--- isimpleaudiovolume_windows.go
+++ isimpleaudiovolume_windows.go
@@ -6,6 +6,8 @@ import (
 	"math"
 	"syscall"
 	"unsafe"
+    
+    "github.com/go-ole/go-ole"
 )
 
 func savSetMasterVolume(sav *ISimpleAudioVolume, level float32, eventContext *ole.GUID) (err error) {
